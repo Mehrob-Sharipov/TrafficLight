98d0c006cfe409dbb990662b66352d015ca81143
Mehrob <shmehrob16@gmail.com>
2022-07-26 05:25:08 +0500

    trafficLight

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red
+    case orange
+    case green
+}
 
+class ViewController: UIViewController {
+    @IBOutlet var RedLable: UIView!
+    @IBOutlet var OrangeLable: UIView!
+    @IBOutlet var GreenLable: UIView!
+    
+    
+    @IBOutlet var RGBButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        RedLable.layer.cornerRadius = 80
+        OrangeLable.layer.cornerRadius = 80
+        GreenLable.layer.cornerRadius = 80
+        
+
+        
+        RGBButton.layer.cornerRadius = 18
+   
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func statButton() {
+    
+    
+        RGBButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            GreenLable.alpha = 0.3
+            RedLable.alpha = 1
+            currentLight = .orange
+            
+        case .orange:
+            RedLable.alpha = 0.3
+            OrangeLable.alpha = 1
+            currentLight = .green
+        case .green:
+            OrangeLable.alpha = 0.3
+            GreenLable.alpha = 1
+            currentLight = .red
+        }
+        
+        
+            
+        }
 
 
 }
-
